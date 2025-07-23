a3d5bc0d454677e7a86b8c0e818bb0cf7317678f
T306 <57376135+T306@users.noreply.github.com>
2025-07-23 15:31:06 -0400

    Update fw_l_r_bw.ino

--- fw_l_r_bw.ino
+++ fw_l_r_bw.ino
@@ -10,8 +10,8 @@ bool wRight = digitalRead(7); //Right Whisker
 void setup(){
   // nokia(4);
   Serial.begin(115200);
-  servoLeft.attach(13); // Attach left signal to pin 13
-  servoRight.attach(12); // Attach right signal to pin 12
+  servoLeft.attach(12); // Attach left signal to pin 13
+  servoRight.attach(13); // Attach right signal to pin 12
   pinMode(5, INPUT); // Left Whisker
   pinMode(7, INPUT); // Right Whisker
   pinMode(8, OUTPUT); // Left LED
@@ -25,15 +25,21 @@ void loop(){
   int irLeft = irDetect(9, 10, 38000); // Check for object
   int irRight = irDetect(2, 3, 38000); // Check for object
 
-  if ((irLeft == 0) && (irRight == 0)){
-    botBackward(0.25, 100);
-  } else if (irLeft == 0){
-    botRightTurn(3);
-  } else if (irRight == 0){
-    botLeftTurn(3);
-  } else {
-    botForward(0.25, 100);
-  }
+  botForward(3*12, 100);
+  botRightTurn(12);
+  botForward(2*12, 100);
+  botLeftTurn(12);
+  botForward(43*12, 100);
+
+  // if ((irLeft == 0) && (irRight == 0)){
+  //   botBackward(0.25, 100);
+  // } else if (irLeft == 0){
+  //   botRightTurn(3);
+  // } else if (irRight == 0){
+  //   botLeftTurn(3);
+  // } else {
+  //   botForward(0.25, 100);
+  // }
 
   // wLeft = digitalRead(5);
   // wRight = digitalRead(7);
